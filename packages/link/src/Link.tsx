e33504a02b01dc017d6b40e85d17e99e58ffb0a8
Oscar Watson-Smith <oscar.watson-smith@brighte.com.au>
2022-05-17 09:29:29 +1000

    chore: remove ...props from link

--- packages/link/src/Link.tsx
+++ packages/link/src/Link.tsx
@@ -6,8 +6,8 @@ import { useLinkComponent } from './linkContext';
 
 export type LinkProps = LinkComponentProps;
 
-export const Link = forwardRefWithAs<'a', LinkProps>((props, ref) => {
+export const Link = forwardRefWithAs<'a', LinkProps>(({ href }, ref) => {
   const LinkComponent = useLinkComponent(ref);
 
-  return <Box as={LinkComponent} asElement="a" ref={ref} {...props} />;
+  return <Box as={LinkComponent} asElement="a" ref={ref} href={href} />;
 });
