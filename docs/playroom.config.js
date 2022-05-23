d7f8a4ebec6aaea68589b93aa7d79c566f747721
Luke Bennett <hello@lukebennett.com.au>
2022-05-23 13:12:11 +1000

    Update Playroom config

--- docs/playroom.config.js
+++ docs/playroom.config.js
@@ -1,36 +1,30 @@
 const babelConfig = require('../babel.config');
-const webpack = require('webpack');
+const { ProvidePlugin } = require('webpack');
 
-module.exports = {
+const playroomConfig = {
   title: 'Playroom | Brighte Spark',
   components: './playroom/components.ts',
   outputPath: './public/playroom/',
   baseUrl: '/playroom/',
   exampleCode: `<Text>Hello world</Text>`,
   frameComponent: './playroom/frame.tsx',
-  port: 9000,
-  typeScriptFiles: [
-    '../packages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    '!**/node_modules',
-  ],
-  widths: [320, 768, 1024],
-  webpackConfig: () => ({
+  webpackConfig,
+};
+
+function webpackConfig() {
+  return {
     module: {
       rules: [
         {
           test: /\.tsx?$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: babelConfig,
-          },
+          use: { loader: 'babel-loader', options: babelConfig },
         },
       ],
     },
-    resolve: {
-      extensions: ['.js', '.ts', '.tsx'],
-    },
-    plugins: [new webpack.ProvidePlugin({ process: 'process/browser' })],
-  }),
-};
+    resolve: { extensions: ['.js', '.ts', '.tsx'] },
+    plugins: [new ProvidePlugin({ process: 'process/browser' })],
+  };
+}
+
+module.exports = playroomConfig;
