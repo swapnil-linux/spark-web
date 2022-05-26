f80bd96c2ba40b73bbe6f839ab44d6bc3fb5e109
Matilda Pan <matilda.pan@brighte.com.au>
2022-05-26 16:29:03 +1000

    poc for react-docgen-typescript

---
+++ docs/scripts/generate-props-doc.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const path = require('path');
+
+const docgen = require('react-docgen-typescript').withCompilerOptions(
+  {
+    noErrorTruncation: true,
+    compilerOptions: {
+      esModuleInterop: true,
+      forceConsistentCasingInFileNames: true,
+      isolatedModules: true,
+      jsx: 'react-jsx',
+      module: 'esnext',
+      moduleResolution: 'node',
+      noEmit: true,
+      noUnusedLocals: true,
+      skipLibCheck: true,
+      strict: true,
+      target: 'esnext',
+      types: ['node', 'jest'],
+    },
+    include: ['**/*'],
+    exclude: ['**/dist/**/*', 'docs', '**/node_modules/**/*'],
+  },
+  {
+    propFilter: {
+      skipPropsWithName: ['children'],
+    },
+    shouldExtractValuesFromUnion: true,
+    shouldExtractLiteralValuesFromEnum: true,
+    shouldRemoveUndefinedFromOptional: true,
+  }
+);
+
+// const resolvedPath = path.resolve('../../packages/divider/src/Divider.tsx');
+// const resolvedPath = path.resolve('../../packages/box/src/Box.tsx');
+const resolvedPath = path.resolve('../../packages/button/src/index.ts');
+// const resolvedPath = path.resolve('../../packages/accordion/src/Accordion.tsx');
+// Parse a file for docgen info
+console.log(JSON.stringify(docgen.parse(resolvedPath), null, 2));
